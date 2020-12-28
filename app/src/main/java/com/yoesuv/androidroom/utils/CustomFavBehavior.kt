0198cc37d60a9e2df234e331bef9b42440d4a707
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-12-28 12:24:56 +0700

    :pencil: update material theme

--- app/src/main/java/com/yoesuv/androidroom/utils/CustomFavBehavior.kt
+++ app/src/main/java/com/yoesuv/androidroom/utils/CustomFavBehavior.kt
@@ -7,18 +7,18 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import android.util.AttributeSet
 import android.view.View
 
-class CustomFavBehavior(context: Context?, attrs: AttributeSet?) : androidx.coordinatorlayout.widget.CoordinatorLayout.Behavior<FloatingActionButton>(context, attrs) {
+class CustomFavBehavior(context: Context?, attrs: AttributeSet?) : CoordinatorLayout.Behavior<FloatingActionButton>(context, attrs) {
 
 
     private val height = Utility.getToolbarHeight(context!!)
 
-    override fun layoutDependsOn(parent: androidx.coordinatorlayout.widget.CoordinatorLayout, child: FloatingActionButton, dependency: View): Boolean {
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: FloatingActionButton, dependency: View): Boolean {
         return dependency is AppBarLayout
     }
 
-    override fun onDependentViewChanged(parent: androidx.coordinatorlayout.widget.CoordinatorLayout, child: FloatingActionButton, dependency: View): Boolean {
+    override fun onDependentViewChanged(parent: CoordinatorLayout, child: FloatingActionButton, dependency: View): Boolean {
         if (dependency is AppBarLayout) {
-            val lp = child.layoutParams as androidx.coordinatorlayout.widget.CoordinatorLayout.LayoutParams
+            val lp = child.layoutParams as CoordinatorLayout.LayoutParams
             val bottomMargin = lp.bottomMargin
             val distanceToScroll = child.height + bottomMargin
             val ratio = dependency.y / height
