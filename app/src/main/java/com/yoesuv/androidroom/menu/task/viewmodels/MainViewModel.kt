0806667eaebe12dcf3797776837fdee10df9ec1f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-12-29 10:29:10 +0700

    :fire: remove old popup

--- app/src/main/java/com/yoesuv/androidroom/menu/task/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/yoesuv/androidroom/menu/task/viewmodels/MainViewModel.kt
@@ -2,23 +2,15 @@ package com.yoesuv.androidroom.menu.task.viewmodels
 
 import android.app.Activity
 import android.app.Application
-import android.app.Dialog
 import androidx.lifecycle.MutableLiveData
 import androidx.room.Room
-import androidx.databinding.DataBindingUtil
-import android.graphics.Point
 import android.os.AsyncTask
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.AndroidViewModel
-import com.yoesuv.androidroom.R
 import com.yoesuv.androidroom.db.TaskDatabase
 import com.yoesuv.androidroom.data.AppConstant
-import com.yoesuv.androidroom.databinding.PopupUpdateTaskBinding
 import com.yoesuv.androidroom.menu.task.AdapterOnClickListener
 import com.yoesuv.androidroom.menu.task.models.MyTaskModel
-import com.yoesuv.androidroom.utils.Utility
 import com.yoesuv.androidroom.utils.dialogInsertUpdateTask
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
@@ -41,17 +33,7 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     }
 
     fun showUpdateTask(activity: Activity, myTask: MyTaskModel, adapterOnClickListener: AdapterOnClickListener){
-        val dialog = Dialog(activity)
-        val popUpBinding: PopupUpdateTaskBinding = DataBindingUtil.inflate(LayoutInflater.from(activity), R.layout.popup_update_task, null, false)
-        popUpBinding.popUpUpdateTask = PopUpUpdateTaskViewModel(dialog, myTask, taskDatabase, this, adapterOnClickListener)
-        dialog.setContentView(popUpBinding.root)
-        val size:Point = Utility.getScreenSize(activity)
-        val width = size.x
-        val widthReduce = (width * 10) /100
-        val widthShow = width - widthReduce
-        dialog.window?.setLayout(widthShow, ViewGroup.LayoutParams.WRAP_CONTENT)
-        dialog.setCanceledOnTouchOutside(false)
-        dialog.show()
+        dialogInsertUpdateTask(activity)
     }
 
     fun deleteTask(myTask: MyTaskModel){
