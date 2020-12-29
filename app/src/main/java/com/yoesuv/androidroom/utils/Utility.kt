811c83cc790465fc41ece8fe34cce9ceef262826
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-12-29 13:34:23 +0700

    :fire: remove unused class

--- app/src/main/java/com/yoesuv/androidroom/utils/Utility.kt
+++ app/src/main/java/com/yoesuv/androidroom/utils/Utility.kt
@@ -1,23 +1,14 @@
 package com.yoesuv.androidroom.utils
 
-import android.app.Activity
 import android.content.Context
-import android.graphics.Point
 import com.yoesuv.androidroom.R
 
 class Utility {
 
     companion object {
-        fun getScreenSize(activity: Activity): Point{
-            val display = activity.windowManager.defaultDisplay
-            val size = Point()
-            display.getSize(size)
-            return size
-        }
 
         fun getToolbarHeight(context: Context): Int {
-            val styledAttributes = context.theme.obtainStyledAttributes(
-                    intArrayOf(R.attr.actionBarSize))
+            val styledAttributes = context.theme.obtainStyledAttributes(intArrayOf(R.attr.actionBarSize))
             val toolbarHeight = styledAttributes.getDimension(0, 0f).toInt()
             styledAttributes.recycle()
 
