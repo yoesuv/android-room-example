8799e8bb3876e7dd2ce19bf981c6f3ac7dbf3849
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-04-11 05:44:55 +0700

    :arrow_up: upgrade gradle & libraries

--- app/src/main/java/com/yoesuv/androidroom/db/TaskDatabase.kt
+++ app/src/main/java/com/yoesuv/androidroom/db/TaskDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.yoesuv.androidroom.data.AppConstant
 import com.yoesuv.androidroom.menu.task.models.MyTaskModel
 
-@Database(entities = [(MyTaskModel::class)], version = 1, exportSchema = false)
+@Database(entities = [MyTaskModel::class], version = 1, exportSchema = false)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun tasksDaoAccess(): TasksDaoAccess
@@ -25,7 +25,9 @@ abstract class TaskDatabase : RoomDatabase() {
         }
 
         private fun create(context: Context): TaskDatabase{
-            return Room.databaseBuilder(context, TaskDatabase::class.java, AppConstant.DATABASE_NAME).build()
+            return Room.databaseBuilder(context, TaskDatabase::class.java, AppConstant.DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build()
         }
     }
 
