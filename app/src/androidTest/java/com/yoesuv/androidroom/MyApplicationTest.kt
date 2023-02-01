ea8d3b0cf6eaa5f83034a6350e9942116b124ee7
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-02-01 08:34:44 +0700

    :pencil: test popup insert show

--- app/src/androidTest/java/com/yoesuv/androidroom/MyApplicationTest.kt
+++ app/src/androidTest/java/com/yoesuv/androidroom/MyApplicationTest.kt
@@ -1,14 +1,22 @@
 package com.yoesuv.androidroom
 
+import android.content.Context
+import android.os.SystemClock
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
 import com.yoesuv.androidroom.menu.task.views.MainActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 
 /**
@@ -20,11 +28,30 @@ import org.junit.Rule
 @LargeTest
 class MyApplicationTest {
 
+    private val delay = 1000L
+    private lateinit var context: Context
+
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
-    @Test
-    fun useAppContext() {
+    @Before
+    fun register() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+    }
 
+    @Test
+    fun fullFlowTest() {
+        val fab = onView(withId(R.id.fabMain))
+
+        fab.perform(click())
+        onView(withText(context.getString(R.string.insert_new_task))).check(matches(isDisplayed()))
+        SystemClock.sleep(delay)
+        onView(withId(R.id.buttonCancel)).perform(click())
+        SystemClock.sleep(delay)
+
+        fab.perform(click())
+        onView(withText(context.getString(R.string.insert_new_task))).check(matches(isDisplayed()))
+        SystemClock.sleep(delay)
+        //onView(withId(R.id.buttonApply)).perform(click())
     }
 }
