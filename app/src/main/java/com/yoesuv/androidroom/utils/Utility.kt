c7c3f956595d09d08f575dfb0d62c7d0e2240844
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-08 15:49:10 +0700

    :arrow_up: upgrade gradle

--- app/src/main/java/com/yoesuv/androidroom/utils/Utility.kt
+++ app/src/main/java/com/yoesuv/androidroom/utils/Utility.kt
@@ -1,14 +1,13 @@
 package com.yoesuv.androidroom.utils
 
 import android.content.Context
-import com.yoesuv.androidroom.R
 
 class Utility {
 
     companion object {
 
         fun getToolbarHeight(context: Context): Int {
-            val styledAttributes = context.theme.obtainStyledAttributes(intArrayOf(R.attr.actionBarSize))
+            val styledAttributes = context.theme.obtainStyledAttributes(intArrayOf(com.google.android.material.R.attr.actionBarSize))
             val toolbarHeight = styledAttributes.getDimension(0, 0f).toInt()
             styledAttributes.recycle()
 
